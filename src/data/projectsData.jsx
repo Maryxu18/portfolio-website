69350946d238b052739ee4c2e48c55dbca492555
Maryxu18 <maryxyx14@gmail.com>
2022-04-16 20:56:18 -0400

    add DS projects

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,25 +1,77 @@
-export const projectsData =  [
-    {tag: "Software Development",
-    projectLink:"https://bookitapp.me",
-    codeLink:"https://github.com/UTSCC09/project-tm",
-    description: "A web application that helps connect nearby readers and provides a user-friendly interface they can use to share their books and borrow from others in their community.",
-title: "BookIt Web App",
-tech: ["React", "JavaScript", "Express.js", "Node.js","MongoDB","GraphQL"],
-img: "bookit.png"},
+export const projectsData = [
+    {
+      tag: "Software Development",
+      projectLink: "https://bookitapp.me",
+      codeLink: "https://github.com/UTSCC09/project-tm",
+      description:
+        "A web application that helps connect nearby readers and provides a user-friendly interface they can use to share their books and borrow from others in their community.",
+      title: "BookIt Web App",
+      tech: [
+        "React",
+        "JavaScript",
+        "Express.js",
+        "Node.js",
+        "MongoDB",
+        "GraphQL",
+      ],
+      img: "bookit.png",
+    },
 
-{tag: "Software Development",
-projectLink:"https://github.com/Maryxu18/E-learning-Platform",
-codeLink:"https://github.com/Maryxu18/E-learning-Platform",
-description: "A website like Google Classroom but is improved to enable organic and guided interactions within the user community.",
-title: "E-Learning Platform",
-tech: ["React", "TypeScript", "MongoDB", "Express.js", "Node.js", "REST API"],
-img: "e-learning.png"},
-
-{tag: "Software Development",
-projectLink:"https://github.com/Maryxu18/Zoomer",
-codeLink:"https://github.com/Maryxu18/Zoomer",
-description: "A backend solution that allows you to request rides - matching you with drivers nearby to take you to your destination.",
-title: "Zoomer",
-tech: ["Java", "PostgreSQL", "MongoDB", "Neo4j", "Docker"],
-img: "zoomer.png"},
-]
+    {
+        tag: "Data Science",
+        projectLink: "https://github.com/richardye101/ubineer_nlp_research",
+        codeLink: "https://richardye101.github.io/ubineer_nlp_research/intro.html",
+        description:
+          "A research project that focuses on unsupervised analysis of textual data from 10K financial reports",
+        title: "Financial Textual Analysis",
+        tech: [
+          "Python",
+          "Sklearn",
+          "GCP",
+        ],
+        img: "textual_analysis.webp",
+    },
+  
+    {
+      tag: "Software Development",
+      projectLink: "https://github.com/Maryxu18/E-learning-Platform",
+      codeLink: "https://github.com/Maryxu18/E-learning-Platform",
+      description:
+        "A website like Google Classroom but is improved to enable organic and guided interactions within the user community.",
+      title: "E-Learning Platform",
+      tech: [
+        "React",
+        "TypeScript",
+        "MongoDB",
+        "Express.js",
+        "Node.js",
+        "REST API",
+      ],
+      img: "e-learning.png",
+    },
+  
+    {
+      tag: "Software Development",
+      projectLink: "https://github.com/Maryxu18/Zoomer",
+      codeLink: "https://github.com/Maryxu18/Zoomer",
+      description:
+        "A backend solution that allows you to request rides - matching you with drivers nearby to take you to your destination.",
+      title: "Zoomer",
+      tech: ["Java", "PostgreSQL", "MongoDB", "Neo4j", "Docker"],
+      img: "zoomer.png",
+    },
+    {
+        tag: "Data Science",
+        projectLink: "",
+        codeLink: "",
+        description:
+          "A research project that aims to improve APOPO’s rat scent detection technology by using stastical analysis and clustering techniques.",
+        title: "APOPO Rat Optimization",
+        tech: [
+          "R",
+          "ggplot",
+        ],
+        img: "apopo.jpeg",
+    },
+  ];
+  
